e97efbca9acdb63c5af472676d517d315fac4ebf
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2022-02-02 13:41:40 +0700

    add registerUser controller, use bcrypt to hash passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -29,5 +30,20 @@ const userSchema = mongoose.Schema({
     default: 'user',
   },
 });
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return;
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  const isMatch = await bcrypt.compare(enteredPassword, this.password);
+
+  return isMatch;
+};
 
 module.exports = mongoose.model('User', userSchema);
