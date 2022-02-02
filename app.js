2bbf0e3359b1da277ee6aa7adb0f01b8c62e36fe
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2022-02-02 15:50:35 +0700

    set up user routes structure

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const connectDB = require('./db/connect');
 
 // routers
 const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
 // middleware
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -37,6 +38,7 @@ app.get('/api/v1', (req, res) => {
 });
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
