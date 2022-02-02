8537c86889eeb85a6d10e3d6471cc12f67c56532
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2022-02-02 11:06:55 +0700

    set up GET route, add 404 and errorHandler middlewares

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 require('dotenv').config();
+require('express-async-errors');
+
+// express
 const express = require('express');
 const app = express();
 
 // database
 const connectDB = require('./db/connect');
+// middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('cbfghgj');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
-
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
